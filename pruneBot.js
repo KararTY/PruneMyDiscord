f871a2dab6a74f23b7f6cc49bee1e6b66469066c
Karar Al-Remahy <KararTY@users.noreply.github.com>
2017-07-30 21:50:34 +0200

    DM channel support.

--- pruneBot.js
+++ pruneBot.js
@@ -4,33 +4,51 @@ const client = new Client()
 // Your auth token.
 let authToken = ''
 
+// Delete a DM channel. 
+let dmChannel = true
+
 // The guild you want to delete your messages in.
 let guildID = ''
 
 // Leave this empty if you want to delete all of your messages in all guild channels.
+// Fill this out if dmChannel is 'true', this is the id of the recipient user.
 let channelID = ''
 
 let count = 0
 
 client.once('ready', () => {
   console.log('Logged into Discord.')
-  Promise.all(client.guilds.get(guildID)
-    .channels.map(channel => {
-      if (channelID) {
-        if (channel.id === channelID && channel.permissionsFor(client.user.id).has('READ_MESSAGES') && channel.permissionsFor(client.user.id).has('SEND_MESSAGES') && channel.permissionsFor(client.user.id).has('READ_MESSAGE_HISTORY') && channel.type === 'text') {
-          console.log(channel.id, channel.name)
-          return fetchMore(channel)
+  // client.user.setGame('Deleting messages...')
+  if (!dmChannel) {
+    Promise.all(client.guilds.get(guildID)
+      .channels.map(channel => {
+        if (channelID) {
+          if (channel.id === channelID && channel.permissionsFor(client.user.id).has('READ_MESSAGES') && channel.permissionsFor(client.user.id).has('SEND_MESSAGES') && channel.permissionsFor(client.user.id).has('READ_MESSAGE_HISTORY') && channel.type === 'text') {
+            console.log(channel.id, channel.name)
+            return fetchMore(channel)
+          }
+        } else {
+          if (channel.permissionsFor(client.user.id).has('READ_MESSAGES') && channel.permissionsFor(client.user.id).has('SEND_MESSAGES') && channel.permissionsFor(client.user.id).has('READ_MESSAGE_HISTORY') && channel.type === 'text') {
+            console.log(channel.id, channel.name)
+            return fetchMore(channel)
+          }
         }
-      } else {
-        if (channel.permissionsFor(client.user.id).has('READ_MESSAGES') && channel.permissionsFor(client.user.id).has('SEND_MESSAGES') && channel.permissionsFor(client.user.id).has('READ_MESSAGE_HISTORY') && channel.type === 'text') {
-          console.log(channel.id, channel.name)
+      })).then(() => {
+      console.log('Done')
+      // process.exit(0) Buggy.
+    })
+  } else {
+    // DM Channel
+    Promise.all(client.channels.map(channel => {
+      if (channelID) {
+        if (channel.type === 'dm' && channel.recipient.id === channelID) {
           return fetchMore(channel)
         }
       }
     })).then(() => {
-    console.log('Done')
-    // process.exit(0) Buggy.
-  })
+      console.log('Done')
+    })
+  }
 })
 
 const fetchMore = (channel, before) => {
@@ -40,6 +58,7 @@ const fetchMore = (channel, before) => {
         let msgLast
         if (msg.last().author.id === client.user.id) msgLast = msg.array()[msg.array().length - 1].id
         else msgLast = msg.last().id
+        // console.log(msgLast)
         msg.forEach(msg => {
           if (msg.author.id === client.user.id) {
             let channelID = msg.channel.id
